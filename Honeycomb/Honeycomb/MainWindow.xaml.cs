0ad95c33d3f9f618571af82a37b2de06b4c25803
8ezhikov <8ezhikov@gmail.com>
2014-10-14 00:30:36 +0400

    Adding service

--- Honeycomb/Honeycomb/MainWindow.xaml.cs
+++ Honeycomb/Honeycomb/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,29 @@ namespace Honeycomb
     /// </summary>
     public partial class MainWindow : Window
     {
+        ServiceHost host;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("http://localhost:22222/chatservice");
+            host = new ServiceHost(typeof(Chatters.ChatService), uri);
+            host.Open();
+            //Console.WriteLine("Chat service listen on endpoint {0}", uri.ToString());
+            //Console.WriteLine("Press ENTER to stop chat service...");
+            //Console.ReadLine();
+     
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            host.Abort();
+            host.Close(); 
+
         }
     }
 }
