c8ae49ddaa4f48ea536ec52fe3ba089fb77491e2
Denis <dchernobrovkin@dataart.com>
2016-01-19 00:12:03 +0300

    Switch to MVVM

---
+++ Honeycomb/CrawlerClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+using GalaSoft.MvvmLight;
+
+namespace CrawlerClient.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+
+        private string _publicAdress;
+        public string IPAddress
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_publicAdress))
+                {
+                    _publicAdress = ClientHelper.GetPublicIP();
+                }
+               
+                return _publicAdress;
+            }
+        }
+    }
+}
