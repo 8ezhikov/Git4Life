92c0ff3d819f644fe08c4bcf29117b58dc091a0f
Denis <dchernobrovkin@dataart.com>
2016-10-31 19:58:48 +0300

    Another one

--- Honeycomb/CrawlerClient/MainWindow.xaml.cs
+++ Honeycomb/CrawlerClient/MainWindow.xaml.cs
@@ -1,24 +1,13 @@
-﻿using System.Windows;
-using Honeycomb;
-
-namespace CrawlerClient
+﻿namespace CrawlerClient
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
-        private string LocalIP;
-        private string GlobalIP;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void StartCrawlerButton_Click(object sender, RoutedEventArgs e)
-        {
-            var crawlerInstance = new CrawlerEngine();
-            crawlerInstance.StartCrawlingProcess("http://webometrics.krc.karelia.ru/");
         }
     }
 }
